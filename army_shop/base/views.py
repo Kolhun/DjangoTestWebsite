fedc11c1cd4027cf0e572afeea98297da96fda9b
Kolhun <36815965+Kolhun@users.noreply.github.com>
2024-08-01 17:56:57 +0300

    less to request look, begin work)

--- army_shop/base/views.py
+++ army_shop/base/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
-    return HttpResponse("Мой первый сайт")
+    return HttpResponse("<h2>Мой первый сайт</h2>")
+
+def categories(request, catid):
+
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h2>Проверка категории</h2><p>{catid}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound(f"<h2>Страница не найдена</h2> <p>{exception}</p>")
